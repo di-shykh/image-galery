2e0d81bba597ad72ca293825f81e2eea0def0b18
di-shykh <di49@mail.ru>
2024-09-28 02:01:20 +0300

    feat: add images to html

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
-const url = "https://api.unsplash.com/search/photos?query=spring&per_page=30&orientation=landscape&client_id=eTG7VANgoQMpa-GgtUa_3LLMy-bLBbsFWjgEGZUUymA";
+const url = "https://api.unsplash.com/search/photos?query=spring&per_page=30&orientation=landscape&extras=url_m&client_id=eTG7VANgoQMpa-GgtUa_3LLMy-bLBbsFWjgEGZUUymA";
 
 async function getData() {
   const res = await fetch(url);
   const data = await res.json();
-  console.log(data);
+  //console.log(data);
+  showImages(data);
 }
 getData();
+//console.log(data[0].urls.regular);
+function showImages(data) {
+  // console.log(data.results[0].urls.regular);
+  const containerForImages = document.querySelector('.image-container .container');
+  data.results.forEach(item => {
+    let imgElement = document.createElement('img')
+    imgElement.src = `${item.urls.regular}`;
+    containerForImages.appendChild(imgElement);
+    // containerForImages.innerHTML = `<img src="${item.urls.regular}" alt="image">`
+  });
+}
